92250049df23ac9a99dbac520f4c69906f6d232c
brensangura <sangura.bren@gmail.com>
2025-04-10 12:43:32 +0300

    happy coding

---
+++ app.py
@@ -0,0 +1,19 @@
+print("Hello World")
+print("*" * 10)
+
+print("Hello World")
+
+print(2 + 2)
+
+x = 1
+
+y = 2
+unit_price = 5.99
+
+students_count = 1000
+print(students_count)
+
+is_published = True
+
+course_name = "Python_Programing"
+print(course_name)
